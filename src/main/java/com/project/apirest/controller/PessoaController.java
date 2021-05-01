8b942a71cf5c4e8912773dc3eab14c3eb6fc76d3
Matheus Guermandi <matheus_guermandi@hotmail.com>
2021-05-01 00:48:21 -0300

    add: get by id Pessoa

--- src/main/java/com/project/apirest/controller/PessoaController.java
+++ src/main/java/com/project/apirest/controller/PessoaController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,6 +40,12 @@ public class PessoaController {
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<Pessoa> getPessoaById(@PathVariable("id") Long id) {
+		Pessoa pessoaById = service.getPessoaById(id);
+        return ResponseEntity.ok().body(pessoaById);
     }
 
 }
